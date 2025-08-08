ea7a6ff0a0f67771887a3fc630ebb523930df2b3
MarcosCruzOn <marcoscruzon@gmail.com>
2025-08-08 19:30:29 -0300

    feat: ensure stripe session is create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ const Home = async () => {
       <div className="space-y-6">
         <div className="px-5">
           <Image
-            src="/banner-01.png"
+            src="/Frame 1984077978.svg"
             alt="Leve uma vida com estilo"
             height={0}
             width={0}
@@ -45,7 +45,7 @@ const Home = async () => {
 
         <div className="px-5">
           <Image
-            src="/banner-02.png"
+            src="/Frame 1984077980.svg"
             alt="Leve uma vida com estilo"
             height={0}
             width={0}
